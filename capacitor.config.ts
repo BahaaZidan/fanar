4db3d789d5df2e65faf08f083201e589719c3eb9
BahaaZidan <gebnatorky@gmail.com>
2025-12-05 21:32:35 +0200

    not ideal but better than I expected

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
 	appId: 'gg.gebna.fanar',
 	appName: 'fanar',
-	webDir: 'build'
+	webDir: 'build',
+	// SHOULD ONLY APPLY DURING DEV
+	server: {
+		url: 'http://192.168.1.100:5169/',
+		cleartext: true
+	}
 };
 
 export default config;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Dialog } from '@capacitor/dialog';
+
+	const showAlert = async () => {
+		await Dialog.alert({
+			title: 'Stop',
+			message: 'this is an error'
+		});
+	};
+</script>
+
+<h1 class="text-2xl">Welcome to FANAR</h1>
+<p>
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentationnnnnn
+</p>
+
+<h2 class="mt-12 text-6xl">niga ???</h2>
+<button class="size-11 text-5xl" onclick={showAlert}>boom</button>
