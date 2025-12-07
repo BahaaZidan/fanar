31e9e283d52ee8ac7a234f4201ad642d566eaf85
BahaaZidan <gebnatorky@gmail.com>
2025-12-07 19:13:57 +0200

    remove splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,21 +1,14 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-	appId: 'gg.gebna.fanar',
-	appName: 'فنار',
-	webDir: 'build',
+	appId: "gg.gebna.fanar",
+	appName: "فنار",
+	webDir: "build",
 	// TODO: SHOULD ONLY APPLY DURING DEV
 	// server: {
-	// 	url: 'http://192.168.1.100:5169/',
-	// 	cleartext: true
+	// 	url: "http://192.168.1.100:5169/",
+	// 	cleartext: true,
 	// },
-	plugins: {
-		SplashScreen: {
-			launchShowDuration: 1000,
-			launchAutoHide: true,
-			launchFadeOutDuration: 1000
-		}
-	}
 };
 
 export default config;
